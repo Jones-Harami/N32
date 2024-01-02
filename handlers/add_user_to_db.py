b17eec5fdfa1e21d3db332ae01274c0a6e4171f8
Jones-Harami <154979125+Jones-Harami@users.noreply.github.com>
2024-01-02 12:44:33 +0530

    Update add_user_to_db.py

--- handlers/add_user_to_db.py
+++ handlers/add_user_to_db.py
@@ -5,7 +5,6 @@ from handlers.database import db
 from pyrogram import Client
 from pyrogram.types import Message
 
-
 async def add_user_to_database(bot: Client, cmd: Message):
     if not await db.is_user_exist(cmd.from_user.id):
         await db.add_user(cmd.from_user.id)
@@ -18,16 +17,6 @@ async def add_user_to_database(bot: Client, cmd: Message):
 async def only_admin_access(bot: Client, cmd: Message):
     user_id = cmd.from_user.id
     if Config.OTHER_USERS_CAN_SAVE_FILE is False:
-        if Config.LOG_CHANNEL is not None:
-            await bot.send_message(
-                int(Config.LOG_CHANNEL),
-                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
-            )
-            return
+        return
     elif user_id in Config.ADMIN:
-        if Config.LOG_CHANNEL is not None:
-            await bot.send_message(
-                int(Config.LOG_CHANNEL),
-                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
-            )
-            return
+        return 
