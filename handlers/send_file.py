bd16f7ced841e002b87ca13c0e4f86ccb6929bfa
Jones-Harami <154979125+Jones-Harami@users.noreply.github.com>
2024-01-02 11:30:10 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -1,8 +1,5 @@
 import asyncio
-import requests
-import string
-import random
-from info import FORWARD_AS_COPY, DB_CHANNEL
+from configs import Config
 from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
@@ -16,16 +13,16 @@ async def reply_forward(message: Message, file_id: int):
             quote=True
         )
     except FloodWait as e:
-        await asyncio.sleep(e.x)
+        await asyncio.sleep(e.value)
         await reply_forward(message, file_id)
 
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
-        if FORWARD_AS_COPY is True:
-            return await bot.copy_message(chat_id=user_id, from_chat_id=DB_CHANNEL,
+        if Config.FORWARD_AS_COPY is True:
+            return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                           message_id=file_id)
-        elif FORWARD_AS_COPY is False:
-            return await bot.forward_messages(chat_id=user_id, from_chat_id=DB_CHANNEL,
+        elif Config.FORWARD_AS_COPY is False:
+            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                               message_ids=file_id)
     except FloodWait as e:
         await asyncio.sleep(e.value)
