2cca1bf6250ebb61fd3c3b5e2ed24d158e777054
Jones-Harami <154979125+Jones-Harami@users.noreply.github.com>
2024-01-03 14:35:42 +0530

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -11,10 +11,11 @@ async def handle_user_status(bot, cmd):
     chat_id = cmd.from_user.id
     if not await db.is_user_exist(chat_id):
         await db.add_user(chat_id)
-        await bot.send_message(
-            Config.LOG_CHANNEL,
-            f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
-        )
+        if Config.LOG_CHANNEL is not None:
+            await bot.send_message(
+                int(Config.LOG_CHANNEL),
+                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
+            )
 
     ban_status = await db.get_ban_status(chat_id)
     if ban_status["is_banned"]:
